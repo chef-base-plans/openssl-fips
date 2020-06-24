3518dedc4ccd92b64e93cc756fa23908a35f676d
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-06-24 23:56:56 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/openssl-fips_exists.rb
+++ controls/openssl-fips_exists.rb
@@ -17,8 +17,9 @@ control 'core-plans-openssl-fips-exists' do
   end
 
   command_relative_path = input('command_relative_path', value: 'bin/fips_standalone_sha1')
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  command_full_path = File.join(plan_installation_directory.stdout.strip, command_relative_path)
   describe file(command_full_path) do
     it { should exist }
+    it { should be_executable }
   end
 end
--- controls/openssl-fips_works.rb
+++ controls/openssl-fips_works.rb
@@ -7,8 +7,8 @@ control 'core-plans-openssl-fips-works' do
   impact 1.0
   title 'Ensure openssl-fips works as expected'
   desc '
-  Verify openssl-fips by ensuring 
-  (1) its installation directory exists and 
+  Verify openssl-fips by ensuring that
+  (1) its installation directory exists 
   (2) the bin/fips_standalone_sha1 returns an expected fips sha for a test file,
       the ./hooks/run.
   '
@@ -21,7 +21,7 @@ control 'core-plans-openssl-fips-works' do
   end
   
   command_relative_path = input('command_relative_path', value: 'bin/fips_standalone_sha1')
-  command_full_path = File.join(plan_installation_directory.stdout.strip, "#{command_relative_path}")
+  command_full_path = File.join(plan_installation_directory.stdout.strip, command_relative_path)
   target_file = File.join(plan_installation_directory.stdout.strip, "hooks/run")
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
   describe command("#{command_full_path} #{target_file}") do
